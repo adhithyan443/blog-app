2446d001264a6805521a3dcfdbf945860f48ff57
adhithyan443 <adhithyanr329@gmail.com>
2026-07-04 14:52:30 +0530

    .env added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBHqxRQ8U1ajvdcZlVpr_wRHk-KC62owvQ",
-    authDomain: "my-blog-app-82868.firebaseapp.com",
-    projectId: "my-blog-app-82868",
-    storageBucket: "my-blog-app-82868.firebasestorage.app",
-    messagingSenderId: "379169170992",
-    appId: "1:379169170992:web:9342d8d10871a4ee216ff5"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
